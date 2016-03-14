df2b886683d794be038c6a615e1830da6c8c4fb2
Dominic Umbeer <dominic@dominicumbeer.de>
2016-03-14 09:28:22 +0100

    Refactored structure and added React specific rules

--- react.js
+++ react.js
@@ -1,7 +1,8 @@
 module.exports = {
   extends: [
     require.resolve('eslint-config-airbnb/base'),
-    require.resolve('./rules/default')
+    require.resolve('./rules/default'),
+    require.resolve('./rules/react')
   ],
   rules: {}
 };
--- rules/default.js
+++ rules/default.js
@@ -1,7 +1,14 @@
 module.exports = {
+  env: {
+    'es6': true,
+    'node': true,
+    'mocha': true,
+  },
+  parser: require.resolve('babel-eslint'),
   'rules': {
     'prefer-arrow-callback': 0,
     'arrow-body-style': 0,
+    'array-bracket-spacing': [2, "always"],
     'comma-dangle': [2, 'never'],
     'space-before-function-paren': [ 2, { anonymous: 'always', named: 'never' } ],
     'object-curly-spacing': [ 2, 'always' ],
